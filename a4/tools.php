3547988d5edfbd847d73ade7e2c8289c3454c93a
Snopel <s3541341@student.rmit.edu.au>
2019-10-10 08:34:38 +1100

    Commenced tools.php as a starting point

--- a4/tools.php
+++ a4/tools.php
@@ -3,4 +3,118 @@
 
 // Put your PHP functions and modules here
 
+$moviesObject = [
+    'AEG' => [
+        'title' => 'Avengers: Endgame',
+        'rating' => 'M',
+        'description' => '<p>Adrift in space with no food or water, Tony Stark sends a message to Pepper Potts as his oxygen supply starts to dwindle. Meanwhile, the remaining Avengers -- Thor, Black Widow, Captain America and Bruce Banner -- must figure out a way to bring back their vanquished allies for an epic showdown with Thanos -- the evil demigod who decimated the planet and the universe. ',
+        'screenings' => [
+        'WED' => 'T12',
+        'THU' => 'T12',
+        'FRI' => 'T12',
+        'SAT' => 'T21',
+        'SUN' => 'T21'
+    ]
+    ],
+    'MID' => [ 
+        'title' => 'Midsommar',
+        'rating' => 'R18+',
+        'description' => '<p>Dani and Christian are a young American couple with a relationship on the brink of falling apart. But after a family tragedy keeps them together, a grieving Dani invites herself to join Christian and his friends on a trip to a once-in-a-lifetime midsummer festival in a remote Swedish village. What begins as a carefree summer holiday in a land of eternal sunlight takes a sinister turn when the insular villagers invite their guests to partake in festivities that render the pastoral paradise increasingly unnerving and viscerally disturbing. ',
+        'screenings' => [
+            'WED' => 'T21',
+            'THU' => 'T21',
+            'FRI' => 'T21',
+            'SAT' => 'T18',
+            'SUN' => 'T18'
+    ]
+    ],
+    'TLK' => [ 
+        'title' => 'The Lion King',
+        'rating' => 'PG',
+        'description' => '<p>After the death of Mufasa, King of the pride and father of young Simba, the underhanded and scheming Scar, brother of Mufasa, takes control of the pride with his gang of blood-thirsty hyenas. ',
+        'screenings' => [
+            'MON' => 'T12',
+            'TUE' => 'T12',
+            'WED' => 'T18',
+            'THU' => 'T18',
+            'FRI' => 'T18'
+    ]
+    ],
+    'OUT' => [
+        'title' => 'Once Upon a Time in Hollywood',
+        'rating' => 'MA15+',
+        'description' => '<p>In 1969 Los Angeles, where everything is changing, as TV star Rick Dalton and his longtime stunt double Cliff Booth make their way around an industry they hardly recognize anymore. The ninth film from the writer-director features a large ensemble cast and multiple storylines in a tribute to the final moments of Hollywood’s golden age.',
+        'screenings' => [
+            'MON' => 'T18',
+            'TUE' => 'T18',
+            'SAT' => 'T15',
+            'SUN' => 'T15'
+    ],
+    ],
+];
+
+$pricesObject = [
+  'full' => [
+    'STA' => 19.8,
+    'STP' => 17.5,
+    'STC' => 15.3,
+    'FCA' => 30.0,
+    'FCP' => 27.0,
+    'FCC' => 24.0,
+  ],
+  'disc' => [
+    'STA' => 14.0,
+    'STP' => 12.5,
+    'STC' => 11.0,
+    'FCA' => 24.0,
+    'FCP' => 22.5,
+    'FCC' => 21.0,
+  ]
+];
+
+function isFullOrDiscount( $day, $hour ) { 
+  if ( $day == 'MON' || $day == 'WED' ) 
+     return 'disc';
+}  else {
+    return 'full';
+}
+
+$days = ['MON','TUE', 'WED', 'THU', 'FRI' , 'SAT', 'SUN'];
+$hours = [ 'T12', 'T15', 'T18', 'T21', 'T00' ]; 
+
+foreach ( $days as $day ) {
+  foreach ( $hours as $hour ) {
+    echo '<p>'.$day.' '.$hour.': '.isFullOrDiscount( $day, $hour ).'</p>';
+    // or this → echo "<p>$day $hour: ".isFullOrDiscount( $day, $hour )."</p>";
+  }
+}
+
+function php2js( $arr, $arrName ) {
+  $lineEnd="";
+  echo "<script>\n";
+  echo "/* Generated with A4's php2js() function */";
+  echo "  var $arrName = ".json_encode($arr, JSON_PRETTY_PRINT);
+  echo "</script>\n\n";
+}
+
+function printMyCode() {
+  $lines = file($_SERVER['SCRIPT_FILENAME']);
+  echo "<pre class='mycode'><ol>";
+  foreach ($lines as $line)
+     echo '<li>'.rtrim(htmlentities($line)).'</li>';
+  echo '</ol></pre>';
+}
+
+function preShow( $arr, $returnAsString=false ) {
+  $ret  = '<pre>' . print_r($arr, true) . '</pre>';
+  if ($returnAsString)
+    return $ret;
+  else 
+    echo $ret; 
+}
+
+
+
+
+
 ?>
